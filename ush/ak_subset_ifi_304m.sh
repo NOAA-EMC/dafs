6ab0ad5fd67be4e6a919ec84944161cee4a46915
hsinmulin-NOAA <48441737+hsinmulin-NOAA@users.noreply.github.com>
2025-06-19 14:29:01 -0400

    Add files via upload
    resolve issue #15 (https://github.com/NOAA-EMC/dafs/issues/15)   & #13 (https://github.com/NOAA-EMC/dafs/issues/13)

--- ush/ak_subset_ifi_304m.sh
+++ ush/ak_subset_ifi_304m.sh
@@ -16,63 +16,70 @@ domain=ak
 
 #--------------------------------------------------------------- 
 
-  fname1="${RUN}.t${cyc}z.ifi.icp.${domain}.f${fhr}.grib2"
-  fname2="${RUN}.t${cyc}z.ifi.sld.${domain}.f${fhr}.grib2"
-  fname3="${RUN}.t${cyc}z.ifi.sev.${domain}.f${fhr}.grib2"
+  fname1="${RUN}.t${cyc}z.ifi.icp.3km.${domain}.f${fhr}.grib2"
+  fname2="${RUN}.t${cyc}z.ifi.sld.3km.${domain}.f${fhr}.grib2"
+  fname3="${RUN}.t${cyc}z.ifi.sev.3km.${domain}.f${fhr}.grib2"
 
   #-- ICPRB
   
-  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":ICPRB:" | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
+  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":ICPRB:" | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${dafs_ifi} -GRIB ${COMOUT}/${fname1}
 
   #-- sipd
   
-  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":SIPD:"  | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
+  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":SIPD:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${dafs_ifi} -GRIB ${COMOUT}/${fname2}
 
   #-- icesev
   
-  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
-  # $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":var discipline=0 master_table=2 parmcat=19 parm=37:" | grep -F -f ${PARMdafs}/rrfs.ifi.sub304m.params | \
+  $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
+  # $WGRIB2 ${COMOUT}/${dafs_ifi} -s | grep ":var discipline=0 master_table=2 parmcat=19 parm=37:" | grep -F -f ${PARMdafs}/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${dafs_ifi} -GRIB ${COMOUT}/${fname3}
 
   #================================================================
-  #-- add WMO header
+  #-- add WMO header only at certain forcast hour
   
   parm_dir=${PARMdafs}/wmo
 
-  #-- icprb
+  #-- remove the leading 0"
+  ifhr=$(expr $fhr + 0)
 
-  parmfile=${parm_dir}/grib2.rrfs.ifi.icprb.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname1}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.icp.${domain}.f${fhr}
+  if [ $ifhr = 1 -o  $ifhr = 2 -o  $ifhr = 3 -o  $ifhr = 6 -o  $ifhr = 9 -o  $ifhr = 12 -o  $ifhr = 15 -o  $ifhr = 18 ]; then
 
-  export FORT11=${infile}             # input file 
-  export FORT12=                      # optional index file
-  export FORT51=${outfile}            # output file w/ headers
+     #-- icprb
 
-  tocgrib2 < $parmfile 1>outfile.icprb.f${fhr}.$$
+     parmfile=${parm_dir}/grib2.dafs.ifi.icprb.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname1}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.icp.3km.${domain}.f${fhr}
 
-  #-- sipd
+     export FORT11=${infile}             # input file 
+     export FORT12=                      # optional index file
+     export FORT51=${outfile}            # output file w/ headers
+
+     tocgrib2 < $parmfile 1>outfile.icprb.f${fhr}.$$
+
+     #-- sipd
   
-  parmfile=${parm_dir}/grib2.rrfs.ifi.sipd.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname2}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sld.${domain}.f${fhr}
+     parmfile=${parm_dir}/grib2.dafs.ifi.sipd.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname2}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sld.3km.${domain}.f${fhr}
 
-  export FORT11=${infile}             # input file 
-  export FORT12=                      # optional index file
-  export FORT51=${outfile}            # output file w/ headers
+     export FORT11=${infile}             # input file 
+     export FORT12=                      # optional index file
+     export FORT51=${outfile}            # output file w/ headers
 
-  tocgrib2 < $parmfile 1>outfile.sipd.f${fhr}.$$
+     tocgrib2 < $parmfile 1>outfile.sipd.f${fhr}.$$
 
-  #-- icesev
+     #-- icesev
 
-  parmfile=${parm_dir}/grib2.rrfs.ifi.icesev.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname3}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sev.${domain}.f${fhr}
+     parmfile=${parm_dir}/grib2.dafs.ifi.icesev.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname3}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sev.3km.${domain}.f${fhr}
 
-  export FORT11=${infile}               # input file 
-  export FORT12=                        # optional index file
-  export FORT51=${outfile}              # output file w/ headers
+     export FORT11=${infile}               # input file 
+     export FORT12=                        # optional index file
+     export FORT51=${outfile}              # output file w/ headers
 
-  tocgrib2 < $parmfile 1>outfile.icesev.f${fhr}.$$
+     tocgrib2 < $parmfile 1>outfile.icesev.f${fhr}.$$
+
+  fi
--- ush/conus_subset_ifi_304m.sh
+++ ush/conus_subset_ifi_304m.sh
@@ -22,11 +22,13 @@ mkdir -p ${COMOUT}/wmo
 
 #---------------------------------------------------------------
 #-- upscaling to G130 from 3km data
+
 # -- IFI --
   $WGRIB2 ${COMOUT}/${dafs_ifi} -set_bitmap 1 -set_grib_type c3 \
      -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
      -new_grid_interpolation neighbor \
      -new_grid ${grid_specs_130} ${COMOUT}/${g130file_ifi}
+
 #-- GTG --
   $WGRIB2 ${COMOUT}/${dafs_gtg} -set_bitmap 1 -set_grib_type c3 \
      -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
@@ -35,66 +37,75 @@ mkdir -p ${COMOUT}/wmo
   
   
 #--------------------------------------------------------------- 
-#-- process WMO data
+#-- process IFI upscaling data 
+
   domain="conus"
   fname1="${RUN}.t${cyc}z.ifi.icp.13km.${domain}.f${fhr}.grib2"
   fname2="${RUN}.t${cyc}z.ifi.sld.13km.${domain}.f${fhr}.grib2"
   fname3="${RUN}.t${cyc}z.ifi.sev.13km.${domain}.f${fhr}.grib2"
 
+  #-- subset IFI data at every 304 m & separate files
+
   #-- ICPRB
-  
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":ICPRB:" | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
+
+  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":ICPRB:" | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname1}
 
   #-- sipd
   
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":SIPD:"  | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
+  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":SIPD:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname2}
 
   #-- icesev
   
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
-  # $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":var discipline=0 master_table=2 parmcat=19 parm=37:" | grep -F -f ${FIXdafs}/prdgen/rrfs.ifi.sub304m.params | \
+  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
+  # $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":var discipline=0 master_table=2 parmcat=19 parm=37:" | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
   $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname3}
 
   #================================================================
-  #-- add WMO header
-  
+  #-- add WMO header only at certain forcast hour
+
   parm_dir=${PARMdafs}/wmo
 
-  #-- icprb
+  #-- remove the leading 0"
+  ifhr=$(expr $fhr + 0)
 
-  parmfile=${parm_dir}/grib2.rrfs.ifi.icprb.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname1}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.icp.13km.${domain}.f${fhr}
-
-  export FORT11=${infile}             # input file 
-  export FORT12=                      # optional index file
-  export FORT51=${outfile}            # output file w/ headers
-
-  tocgrib2 < $parmfile 1>outfile.icprb.f${fhr}.$$
-
-  #-- sipd
+  if [ $ifhr = 1 -o  $ifhr = 2 -o  $ifhr = 3 -o  $ifhr = 6 -o  $ifhr = 9 -o  $ifhr = 12 -o  $ifhr = 15 -o  $ifhr = 18 ]; then   
   
-  parmfile=${parm_dir}/grib2.rrfs.ifi.sipd.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname2}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sld.13km.${domain}.f${fhr}
+     #-- icprb
 
-  export FORT11=${infile}             # input file 
-  export FORT12=                      # optional index file
-  export FORT51=${outfile}            # output file w/ headers
+     parmfile=${parm_dir}/grib2.dafs.ifi.icprb.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname1}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.icp.13km.${domain}.f${fhr}
 
-  tocgrib2 < $parmfile 1>outfile.sipd.f${fhr}.$$
+     export FORT11=${infile}             # input file 
+     export FORT12=                      # optional index file
+     export FORT51=${outfile}            # output file w/ headers
 
-  #-- icesev
+     tocgrib2 < $parmfile 1>outfile.icprb.f${fhr}.$$
 
-  parmfile=${parm_dir}/grib2.rrfs.ifi.icesev.${fhr}      # parm file w/ header info
-  infile=${COMOUT}/${fname3}
-  outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sev.13km.${domain}.f${fhr}
+     #-- sipd
+  
+     parmfile=${parm_dir}/grib2.dafs.ifi.sipd.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname2}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sld.13km.${domain}.f${fhr}
 
-  export FORT11=${infile}               # input file 
-  export FORT12=                        # optional index file
-  export FORT51=${outfile}              # output file w/ headers
+     export FORT11=${infile}             # input file 
+     export FORT12=                      # optional index file
+     export FORT51=${outfile}            # output file w/ headers
 
-  tocgrib2 < $parmfile 1>outfile.icesev.f${fhr}.$$
+     tocgrib2 < $parmfile 1>outfile.sipd.f${fhr}.$$
 
+     #-- icesev
+
+     parmfile=${parm_dir}/grib2.dafs.ifi.icesev.${fhr}      # parm file w/ header info
+     infile=${COMOUT}/${fname3}
+     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sev.13km.${domain}.f${fhr}
+
+     export FORT11=${infile}               # input file 
+     export FORT12=                        # optional index file
+     export FORT51=${outfile}              # output file w/ headers
+
+     tocgrib2 < $parmfile 1>outfile.icesev.f${fhr}.$$
+
+  fi
