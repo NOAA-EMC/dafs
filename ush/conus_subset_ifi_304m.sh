0f55252a8fc7db3fa55af15b1e2d8da773f5844b
hsinmulin-NOAA <48441737+hsinmulin-NOAA@users.noreply.github.com>
2025-09-04 20:12:02 -0400

    Add files via upload
    resolve items in issues #35

--- ush/conus_subset_ifi_304m.sh
+++ ush/conus_subset_ifi_304m.sh
@@ -31,22 +31,23 @@ g130file_gtg=dafs.t${cyc}z.gtg.13km.conus.f${fhr}.grib2
 #--- Grid 130 for the GTG & ICING process
 grid_specs_130="lambert:265:25.000000 233.862000:451:13545.000000 16.281000:337:13545.000000"
 
+cd "${COMOUT}" || err_exit "FATAL ERROR: Could not 'cd ${COMOUT}'; ABORT!"
 mkdir -p ${COMOUT}/wmo
 
 #---------------------------------------------------------------
 #-- upscaling to G130 from 3km data
 
 # -- IFI --
-  $WGRIB2 ${COMOUT}/${dafs_ifi} -set_bitmap 1 -set_grib_type c3 \
+  $WGRIB2 ${dafs_ifi} -set_bitmap 1 -set_grib_type c3 \
      -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
      -new_grid_interpolation neighbor \
-     -new_grid ${grid_specs_130} ${COMOUT}/${g130file_ifi}
+     -new_grid ${grid_specs_130} ${g130file_ifi}
 
 #-- GTG --
-  $WGRIB2 ${COMOUT}/${dafs_gtg} -set_bitmap 1 -set_grib_type c3 \
+  $WGRIB2 ${dafs_gtg} -set_bitmap 1 -set_grib_type c3 \
      -new_grid_winds grid -new_grid_vectors "UGRD:VGRD:USTM:VSTM" \
      -new_grid_interpolation bilinear \
-     -new_grid ${grid_specs_130} ${COMOUT}/${g130file_gtg}
+     -new_grid ${grid_specs_130} ${g130file_gtg}
   
   
 #--------------------------------------------------------------- 
@@ -59,23 +60,25 @@ mkdir -p ${COMOUT}/wmo
 
   #-- subset IFI data at every 304 m & separate files
 
+  cpreq ${FIXdafs}/prdgen/dafs.ifi.sub304m.params .
+
   #-- ICPRB
 
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":ICPRB:" | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
-  $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${fname1}
+  $WGRIB2 ${g130file_ifi} -s | grep ":ICPRB:" | grep -F -f dafs.ifi.sub304m.params | \
+  $WGRIB2 -i ${g130file_ifi} -GRIB ${fname1}
   # $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname1}
 
   #-- sipd
   
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":SIPD:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
-  $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${fname2}
+  $WGRIB2 ${g130file_ifi} -s | grep ":SIPD:"  | grep -F -f dafs.ifi.sub304m.params | \
+  $WGRIB2 ${g130file_ifi} -GRIB ${fname2}
   # $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname2}
 
   #-- icesev
   
-  $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
+  $WGRIB2 ${g130file_ifi} -s | grep -E ":ICESEV:|parm=37:"  | grep -F -f dafs.ifi.sub304m.params | \
   # $WGRIB2 ${COMOUT}/${g130file_ifi} -s | grep ":var discipline=0 master_table=2 parmcat=19 parm=37:" | grep -F -f ${FIXdafs}/prdgen/dafs.ifi.sub304m.params | \
-  $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${fname3}
+  $WGRIB2 -i ${g130file_ifi} -GRIB ${fname3}
   # $WGRIB2 -i ${COMOUT}/${g130file_ifi} -GRIB ${COMOUT}/${fname3}
 
   #================================================================
@@ -90,41 +93,86 @@ mkdir -p ${COMOUT}/wmo
   
      #-- icprb
 
-     parmfile=${parm_dir}/grib2.dafs.ifi.icprb.${fhr}      # parm file w/ header info
+     parmfile=grib2.dafs.ifi.icprb.${fhr}      # parm file w/ header info
      infile=${fname1}
      # infile=${COMOUT}/${fname1}
-     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.icp.13km.${domain}.f${fhr}
+     outfile=grib2.dafs.t${cyc}z.ifi.icp.13km.${domain}.f${fhr}
+
+     cpreq ${parm_dir}/${parmfile} .
 
      export FORT11=${infile}             # input file 
      export FORT12=                      # optional index file
      export FORT51=${outfile}            # output file w/ headers
 
-     tocgrib2 < $parmfile 1>outfile.icprb.f${fhr}.$$
+     ${TOCGRIB2} < $parmfile 1>outfile.icprb.f${fhr}.$$
+
+     export err=$?
+     err_chk
+
+     # Check if TOCGRIB2 succeeded in creating the output file
+     if [[ ! -f "${FORT51}" ]]; then
+        err_exit "FATAL ERROR: '${pgm}' failed to create '${FORT51}', ABORT!"
+     fi
+
+     # Send data to COM
+     if [[ "${SENDCOM}" == "YES" ]]; then
+        cpfs ${outfile} ${COMOUT}/wmo
+     fi
 
      #-- sipd
   
-     parmfile=${parm_dir}/grib2.dafs.ifi.sipd.${fhr}      # parm file w/ header info
+     parmfile=grib2.dafs.ifi.sipd.${fhr}      # parm file w/ header info
      infile=${fname2}
      # infile=${COMOUT}/${fname2}
-     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sld.13km.${domain}.f${fhr}
+     outfile=grib2.dafs.t${cyc}z.ifi.sld.13km.${domain}.f${fhr}
+
+     cpreq ${parm_dir}/${parmfile} .
 
      export FORT11=${infile}             # input file 
      export FORT12=                      # optional index file
      export FORT51=${outfile}            # output file w/ headers
 
-     tocgrib2 < $parmfile 1>outfile.sipd.f${fhr}.$$
+     ${TOCGRIB2} < $parmfile 1>outfile.sipd.f${fhr}.$$
+
+     export err=$?
+     err_chk
+
+     # Check if TOCGRIB2 succeeded in creating the output file
+     if [[ ! -f "${FORT51}" ]]; then
+        err_exit "FATAL ERROR: '${pgm}' failed to create '${FORT51}', ABORT!"
+     fi
+
+     # Send data to COM
+     if [[ "${SENDCOM}" == "YES" ]]; then
+        cpfs ${outfile} ${COMOUT}/wmo
+     fi
 
      #-- icesev
 
-     parmfile=${parm_dir}/grib2.dafs.ifi.icesev.${fhr}      # parm file w/ header info
+     parmfile=grib2.dafs.ifi.icesev.${fhr}      # parm file w/ header info
      infile=${fname3}
      # infile=${COMOUT}/${fname3}
-     outfile=${COMOUT}/wmo/grib2.dafs.t${cyc}z.ifi.sev.13km.${domain}.f${fhr}
+     outfile=grib2.dafs.t${cyc}z.ifi.sev.13km.${domain}.f${fhr}
+
+     cpreq ${parm_dir}/${parmfile} .
 
      export FORT11=${infile}               # input file 
      export FORT12=                        # optional index file
      export FORT51=${outfile}              # output file w/ headers
 
-     tocgrib2 < $parmfile 1>outfile.icesev.f${fhr}.$$
+     ${TOCGRIB2} < $parmfile 1>outfile.icesev.f${fhr}.$$
+
+     export err=$?
+     err_chk
+
+     # Check if TOCGRIB2 succeeded in creating the output file
+     if [[ ! -f "${FORT51}" ]]; then
+        err_exit "FATAL ERROR: '${pgm}' failed to create '${FORT51}', ABORT!"
+     fi
+
+     # Send data to COM
+     if [[ "${SENDCOM}" == "YES" ]]; then
+        cpfs ${outfile} ${COMOUT}/wmo
+     fi
 
   fi
